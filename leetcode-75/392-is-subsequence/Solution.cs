3de4471ef1f69299853eea8b40f4a6e9ad6f87ef
UtkuLoptuk <utkuloptuk@gmail.com>
2022-07-14 23:14:02 +0300

    partial 392 problem solved.

--- leetcode-75/392-is-subsequence/Solution.cs
+++ leetcode-75/392-is-subsequence/Solution.cs
@@ -10,6 +10,28 @@ namespace leetcode_75._392_is_subsequence
     {
         public static bool IsSubsequence(string s, string t)
         {
+            var sCharArray = s.ToCharArray().Distinct().ToArray();
+            var tChars= t.ToCharArray().Distinct().ToString();
+            int firstIndex = 0;
+            int SecondIndex = 0;
+            for (int i = 0; i < sCharArray.Count(); i++)
+            {
+                if (t.Contains(sCharArray[i])
+                    &&s.Count(x=>x.Equals(sCharArray[i]))<=t.Count(x=>x.Equals(sCharArray[i])))
+                {
+                    if (i == 0)
+                        continue;
+                    firstIndex = t.LastIndexOf(sCharArray[i - 1]);
+                    SecondIndex = t.LastIndexOf(sCharArray[i]);
+                    if (firstIndex > SecondIndex)
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                    return false;
+            }
             return true;
         }
     }
