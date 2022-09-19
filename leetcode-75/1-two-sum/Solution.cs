916f85886065c3d4d2fc28e5d805d5d207fa0d4f
UtkuLoptuk <utkuloptuk@gmail.com>
2022-09-19 17:21:50 +0300

    added 1- two-sum.

---
+++ leetcode-75/1-two-sum/Solution.cs
@@ -0,0 +1,27 @@
+﻿
+namespace leetcode_75._1_two_sum
+{
+    public static class Solution
+    {
+        public static int[] TwoSumIndex(int[] nums, int target)
+        {
+            Dictionary<int, int> maps = new Dictionary<int, int>();
+            List<int> result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                maps.Add(i, nums[i]);
+                var lists = nums.ToList();
+                lists.RemoveAt(i);
+                var newArray=lists.ToArray();
+                if (newArray.Contains(target - nums[i]))
+                {
+                    result.Add(i);
+                    if (result.Count == 2)
+                        break;
+                }
+
+            }
+            return result.ToArray();
+        }
+    }
+}
---
+++ leetcode-75/1-two-sum/SolutionTest.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace leetcode_75._1_two_sum
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        //[DataRow(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        //[DataRow(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [DataRow(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+
+        public void Is_Two_Sum_Data_Index_Correct(int[] array, int target, int[] resultArray)
+        {
+            var result = Solution.TwoSumIndex(array, target);
+            Assert.IsTrue(resultArray.SequenceEqual(result));
+
+        }
+    }
+}
