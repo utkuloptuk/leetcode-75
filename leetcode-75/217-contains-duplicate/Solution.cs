ca8cd27c2dfc23ac7ba1d87cadbaa6d5ede9518e
UtkuLoptuk <utkuloptuk@gmail.com>
2022-09-21 11:22:59 +0300

    217-contains-duplicate

---
+++ leetcode-75/217-contains-duplicate/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode_75._217_contains_duplicate
+{
+    public static class Solution
+    {
+        public static bool CheckListForDuplicateRecord(int[] list)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for(int i=0; i<list.Length;i++)
+            {
+                if (set.Contains(list[i]))
+                {
+                    return true;
+                }
+                set.Add(list[i]);
+            }
+            return false;
+        }
+    }
+}
---
+++ leetcode-75/217-contains-duplicate/SolutionTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcode_75._217_contains_duplicate
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 1 }, true)]
+        [DataRow(new int[] { 1, 2, 3, 4 }, false)]
+        [DataRow(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+        public void duplicate_contains_check_is_succeed(int[] list, bool expectedResult)
+        {
+            var result = Solution.CheckListForDuplicateRecord(list);
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
