1dfe13b62c9d6b6067f26a4d42e5897a9ec2e4f3
UtkuLoptuk <utkuloptuk@gmail.com>
2022-09-22 11:20:20 +0300

    added 238 product of array except self.

---
+++ leetcode-75/238-product-of-array-except-self/SolutionTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_75._238_product_of_array_except_self
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4 }, new int[] { 24, 12, 8, 6 })]
+        [DataRow(new int[] { -1, 1, 0, -3, 3 }, new int[] { 0, 0, 9, 0, 0 })]
+        public void array_multiply_all_elements_except_self_is_succeed(int[] inputArray, int[] exceptedArray)
+        {
+            var result = Solution.ArrayElementsMultiplyExceptSelf(inputArray);
+            Assert.IsTrue(result.SequenceEqual(exceptedArray));
+        }
+    }
+}
