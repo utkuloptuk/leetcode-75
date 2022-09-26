8c2f68c0fe9f83405b61ba16bd12d907e9b32ea1
UtkuLoptuk <utkuloptuk@gmail.com>
2022-09-26 16:28:48 +0300

    added 153 find rotated sorted min array.

---
+++ leetcode-75/153-find-minimum-rotated-sorted-array/Solution.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace leetcode_75._153_find_minimum_rotated_sorted_array
+{
+    public static class Solution
+    {
+        public static int FindMinValue(int[] sortedArray)
+        {
+            int left = 0;
+            int right = sortedArray.Length - 1;
+            int result = sortedArray[0];
+            int mid = (left + right) / 2;
+            while (left <= right)
+            {    
+                if (sortedArray[left] < sortedArray[right])
+                {
+                    result = Math.Min(result, sortedArray[left]);
+                    break;
+                }
+                result = Math.Min(result, sortedArray[mid]);
+                if (sortedArray[mid] >= sortedArray[left])
+                {
+                    left = mid + 1;
+                    mid++;
+                }
+                else
+                {
+                    right = mid - 1;
+                    mid--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcode-75/153-find-minimum-rotated-sorted-array/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_75._153_find_minimum_rotated_sorted_array
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        [DataRow(new int[] { 3, 4, 5, 1, 2 }, 1)]
+        [DataRow(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0)]
+        [DataRow(new int[] { 11, 13, 15, 17 }, 11)]
+
+        public void Find_Minumum_Value_in_Sorted_Array_is_Succeed(int[] sortedArray, int expectedOutput)
+        {
+            var result = Solution.FindMinValue(sortedArray);
+            Assert.AreEqual(expectedOutput,result);
+        }
+    }
+}
