4db414e9082c950c1491dc6d70c0045248d9f018
UtkuLoptuk <utkuloptuk@gmail.com>
2022-09-29 22:52:53 +0300

    added 15-3sum.

---
+++ leetcode-75/15-3sum/Solution.cs
@@ -0,0 +1,45 @@
+﻿
+namespace leetcode_75._15_3sum
+{
+    public static class Solution
+    {
+        public static int[][] ThreeSum(int[] inputArray)
+        {
+            int left = 0;
+            int length = inputArray.Length;
+            int right = length - 1;
+            int counter = 0;
+            int[][] resultList = new int[][] { };
+            Array.Sort(inputArray);
+            for (int i = 0; i < length; i++)
+            {
+                if (!i.Equals(0) && inputArray[i].Equals(inputArray[i - 1]))
+                {
+                    left++;
+                    continue;
+                }
+                int firstVal = inputArray[i];
+                while (left < right)
+                {
+                    var treeSum = firstVal + inputArray[left] + inputArray[right];
+                    if (treeSum > 0)
+                        right--;
+                    else if (treeSum < 0)
+                        left++;
+                    else
+                    {
+                        var appValue = new int[] { firstVal, inputArray[left], inputArray[right] };
+                        resultList.Append(appValue);
+                        counter++;
+                        break;
+                    }
+                }
+                left = 0;
+                length = inputArray.Length;
+            }
+
+
+            return resultList;
+        }
+    }
+}
