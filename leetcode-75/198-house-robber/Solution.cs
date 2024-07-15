f26f8f94e2879988137188ef0cad8a9974532452
utku topçu <utkuloptuk@gmail.com>
2024-07-15 15:32:40 +0300

    added house robber solution.

--- leetcode-75/198-house-robber/Solution.cs
+++ leetcode-75/198-house-robber/Solution.cs
@@ -4,7 +4,15 @@ public static class Solution
 {
     public static int HouseRobber(int[] nums)
     {
-        return 0;
+        int rob1=0;
+        int rob2 = 0;
+        foreach (int num in nums)
+        {
+            int temp = num + rob1 > rob2 ? num + rob1 : rob2;
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        return rob2;
     }
     
 }
--- leetcode-75/198-house-robber/SolutionTest.cs
+++ leetcode-75/198-house-robber/SolutionTest.cs
@@ -5,6 +5,7 @@ namespace leetcode_75._198_house_robber;
 [TestClass]
 public class SolutionTest
 {
+    [TestMethod]
     public void HouseRobber_is_Succeed_Case_1()
     {
         int[] req = new int[] { 1, 2, 3, 1 };
@@ -12,6 +13,7 @@ public class SolutionTest
         var result = Solution.HouseRobber(req);
         Assert.AreEqual(res,result);
     }
+    [TestMethod]
     public void HouseRobber_is_Succeed_Case_2()
     {
         int[] req = new int[] { 2,7,9,3,1};
